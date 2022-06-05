910ddac532407a2a655380dd25c5b08e437dd026
LidoKing <kinglido203@gmail.com>
2022-06-06 01:30:04 +0800

    app title

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,8 @@ import Form from "./Form";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 id="title">the only salary tracker you need</h1>
+      <Form />
     </div>
   );
 }
